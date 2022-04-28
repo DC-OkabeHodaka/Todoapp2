dcb405660fe1e3a289dce7396ead38e63087ac40
HO <h.okabe@g.dreamcareer.co.jp>
2022-04-28 16:19:45 +0900

    作り直し

---
+++ config.js
@@ -0,0 +1,9 @@
+const dbSetting = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  timrzone: "jst",
+};
+
+exports.dbSetting = dbSetting;
